1b166ebfd0c0cca178d9ba5aa1ff7558c66bbfe7
bitandbrick <makermurtaza@bitandbrick.com>
2024-04-19 14:55:26 +0400

    events

--- pages/api/events/all.js
+++
@@ -1,6 +0,0 @@
-import { getEvents } from '../../../lib/data'
-
-export default async (req, res) => {
-  const events = await getEvents()
-  res.json(events)
-}
--- pages/api/events/upcoming.js
+++
@@ -1,7 +0,0 @@
-import { getEvents } from '../../../lib/data'
-
-export default async (req, res) => {
-  let events = await getEvents('DATETIME_DIFF(start,TODAY()) > 0')
-  events = events.filter(e => (new Date(e.end) > new Date()))
-  res.json(events)
-}
